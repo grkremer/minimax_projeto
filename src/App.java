1c0138d9840c4e437f10b02b136fa8ba379e7352
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2021-06-03 18:25:30 -0300

    ArvoreDeJogadas
    Mudei pra nova arvore

--- src/App.java
+++ src/App.java
@@ -3,15 +3,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         ArvoreDeJogadas j = new ArvoreDeJogadas();
-        j.setData(new Jogada(new int[2][2]));
-        j.getData().calculaPontos();
+        j.calculaPontos();
 
         /*ArrayList<Integer> l = new ArrayList<Integer>();
         l.add(1);
         l.set(0, 3);
         System.out.println(l);
 */
-        System.out.println("Raiz: "+j.getData().getPontos());
+        System.out.println("Raiz: "+j.getPontos());
         j.printaArvore();
     }
 }
--- src/ArvoreDeJogadas.java
+++ src/ArvoreDeJogadas.java
@@ -1,14 +1,54 @@
-public class ArvoreDeJogadas extends Tree<Jogada> {
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArvoreDeJogadas {
+    private int[][] tabuleiro;
+    private int pontos;
+    private List<ArvoreDeJogadas> children;
+
     public ArvoreDeJogadas() {
-        super();
+        this.children = new ArrayList<ArvoreDeJogadas>();
     }
-    private void printaArvore(Tree<Jogada> a){
-        getData().print();
-        for(int i=0; i<getChildren().size(); i++){
-            printaArvore(getChild(i));
-        }
+    public int[][] getTabuleiro() {
+        return tabuleiro;
     }
+    public void setTabuleiro(int[][] tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+    public int getPontos() {
+        return pontos;
+    }
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+    public List<ArvoreDeJogadas> getChildren() {
+        return children;
+    }
+    public void setChildren(List<ArvoreDeJogadas> children) {
+        this.children = children;
+    }
+    public ArvoreDeJogadas getChild(int index) {
+        return children.get(index);
+    }
+    public void setChild(int index, ArvoreDeJogadas children) {
+        this.children.set(index, children);
+    }
+    public void addChild(ArvoreDeJogadas children) {
+        this.children.add(children);
+    }
+    private int randomInt(int min, int max){
+        Random random = new Random();
+        return random.ints(min,(max+1)).findFirst().getAsInt();
+    }
+    public void calculaPontos(){
+        setPontos(randomInt(-10, 10));
+    }
+
     public void printaArvore(){
-        printaArvore(this);
+        System.out.println(pontos+" ");
+        for(int i=0; i<getChildren().size(); i++){
+            getChild(i).printaArvore();
+        }
     }
 }
